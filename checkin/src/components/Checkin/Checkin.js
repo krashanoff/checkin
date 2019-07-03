b482468a8e44fd6f02afd303230034fbe3372f39
krashanoff <lkrashanoff2@gmail.com>
2019-07-02 22:44:24 -0700

    Add userID field to Checkin's state.

--- checkin/src/components/Checkin/Checkin.js
+++ checkin/src/components/Checkin/Checkin.js
@@ -2,16 +2,27 @@ import React from 'react';
 import './Checkin.css';
 import queryString from 'query-string';
 
+const API_KEY = process.env.WILDAPRICOT_KEY;
+
 class Checkin extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userID: ''
+        };
+    }
+
     componentDidMount() {
-        const userID = queryString.parse(this.props.location.search);
-        console.log('entered the checkin page. Arguments passed to the URL are ' + userID.id);
+        this.setState({userID: queryString.parse(this.props.location.search).id});
+        console.log('entered the checkin page. Arguments passed to the URL are ' + this.state.userID);
+        console.log(API_KEY);
     }
 
     render() {
         return (
             <div className='checkin'>
-                This is the check in page.
+                This is the check in page. Searching for id {this.state.userID}
             </div>
         );
     }
