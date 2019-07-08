8ca211f039917502a6d2105345146308492625a9
krashanoff <lkrashanoff2@gmail.com>
2019-07-07 17:30:32 -0700

    Create global color scheme CSS file.

--- checkin/src/index.js
+++ checkin/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import './globals.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
