cfddbc1fd8bcd7fa5589b1196cc0a82653f3d518
krashanoff <lkrashanoff2@gmail.com>
2019-07-07 20:16:16 -0700

    Add suggestion styling. Allow for proper rendering of suggestions based on component state. Update app manifest. Add more global variables.

--- checkin/src/components/Start/Start.js
+++ checkin/src/components/Start/Start.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Start.css';
 
+// dictates the minimum amount required to input before we
+// start parsing for suggestions.
 const SEARCHMIN = 3;
 
 /* TODO:
- *  - Reduce number of API requests made overall.
+ *  - IDEA: Make the lastNames state fields of type pair<int[], string>,
+ *          this way we can have our ids bound to each last name.
+ *  - Q: Ask if we are designing for pool members to check themselves in,
+ *       or whether we are designing for lifeguards for it to be easier to
+ *       check guests in.
  */
 class Suggestions extends React.Component {
     constructor(props) {
@@ -13,13 +19,14 @@ class Suggestions extends React.Component {
         // declare all necessary fields for parsing
         this.state = {
             value: '',
-            data: '',
-            ids: '',
-            lastNamesAll: '',
-            lastNamesVisible: ''
+            data: [],
+            ids: [],
+            lastNamesAll: [],
+            lastNamesVisible: []
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.click = this.click.bind(this);
     }
 
     /* handleChange
@@ -33,6 +40,8 @@ class Suggestions extends React.Component {
         if (event.target.value.length < SEARCHMIN)
             return;
 
+        // when we reach the minimum search query length, we request
+        // data from the API and begin our suggestions.
         if (event.target.value.length === SEARCHMIN) {
             // fetch data from the API
             this.setState({data : [
@@ -185,29 +194,55 @@ class Suggestions extends React.Component {
             });
 
             // eliminate duplicate entries
-            for (var i = 0; i < names.length; i++)
-                for (var k = i + 1; k < names.length; k++)
+            for (var i = 0; i < names.length; i++) {
+                for (var k = i + 1; k < names.length; k++) {
                     if (names[i] === names[k]) {
                         names.splice(k);
                         break;
                     }
+                }
+            }
+
+            // update the current state to reflect the new information
+            this.setState({
+                lastNamesAll: names,
+                lastNamesVisible: names
+            });
         }
 
         // Based on the current input, limit the number of visible results from
         // previously parsed data. This is stored in state.lastNamesVisible.
         // This limits our number of overall API requests per check-in.
+
+        // for every last name we currently have available given the current input:
+        for (var i = 0; i < this.state.lastNamesAll.length; i++) {
+            // for every character in the name:
+            for (var k = 0; k < this.state.lastNamesAll[i].length; k++) {
+                // TODO: check that it is actually a suggestion.
+            }
+        }
+    }
+
+    /* click
+     * Redirect the user to the proper page based on which entry they click
+     * and the current state of the component.
+     */
+    click(event) {
+        // TODO: Write this.
+        const uid = event.target.getAttribute('uid');
+        console.log(uid);
     }
 
     render() {
-        /* parse our current data to render the suggestions
-         *  - If duplicate last name entries, associate their ids to the same
-         *    last name.
-         */
+        // parse our current data to render the suggestions
         var names = [];
-        for (var i = 0; i < this.state.lastNamesVisible.length; i++) {
-            const name = this.state.lastNamesVisible[i];
-            names.push(<div className='suggestion' onClick={this.click} key={name.uid}>{name}</div>);
-        }
+        var i = 0;          // simply used for the key field.
+        
+        // TODO: Update so that uid is the actual associated uid information.
+        Array.from(this.state.lastNamesVisible).forEach( (name) => {
+            names.push(<div className='suggestion' onClick={this.click} uid={name} key={'suggestion' + i}>{name}</div>);
+            i++;
+        });
 
         return(
             <div id='suggestions'>
